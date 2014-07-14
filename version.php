89124d30d968aeeb807df1613cb0464997295599
Goran Josic <goran.josic@usi.ch>
2014-07-14 11:20:16 +0200

    Upgrade istructions updated and Moodle 2.7 restriction

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 $plugin->component  = 'block_notifications';
 $plugin->version  = 2014040404;
 $plugin->release  = 1;
-$plugin->requires  =2011033003;
+$plugin->requires  = 2014052000;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron  = 1;
 ?>
