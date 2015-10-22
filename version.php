933bbafa7aa2f5e90ca39003fadce5e925af2f8c
Goran Josic <goran.josic@usi.ch>
2015-10-22 15:50:59 +0200

    Filter suspended users
    Don't send notifications to the suspended users

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 $plugin->component  = 'block_notifications';
-$plugin->version  = 2015052800;
+$plugin->version  = 2015102200;
 $plugin->release  = 1;
 $plugin->requires  = 2014051200;
 $plugin->maturity  = MATURITY_STABLE;
